bfdbb321724110a896cf770de1c5a099beeb338a
Greg Muszynski <g.muszynski.12@abdn.ac.uk>
2015-11-02 20:42:47 +0000

    Showing more numbers

--- coverage.rb
+++ coverage.rb
@@ -1,29 +1,43 @@
 require_relative 'computational_sentiment_classifier'
 
 class Coverage
-  def self.lexicon_coverage
+  def self.stats
     csc = CSC.new
     nokia_positive = File.open("nokia-pos.txt").read
     nokia_negative = File.open("nokia-neg.txt").read
 
-    all_words = nokia_positive.split.keep_if { |w| (w =~ /\w/) }
-    lexicon_coverage_positive = all_words.select do |w|
+    all_words_positiv = nokia_positive.split.keep_if { |w| (w =~ /\w/) }
+    positiv_hits = all_words_positiv.select do |w|
       csc.sent_lex.lexicon[w] || csc.sent_lex.lexicon[csc.lemmatizer.lemma(w)]
-    end.count / all_words.count.to_f
+    end.count
+    positiv_coverage = positiv_hits / all_words_positiv.count.to_f
 
-    all_words = nokia_negative.split.keep_if { |w| (w =~ /\w/) }
-    lexicon_coverage_negative = all_words.select do |w|
+    all_words_negativ = nokia_negative.split.keep_if { |w| (w =~ /\w/) }
+    negativ_hits = all_words_negativ.select do |w|
       csc.sent_lex.lexicon[w] || csc.sent_lex.lexicon[csc.lemmatizer.lemma(w)]
-    end.count / all_words.count.to_f
+    end.count
+    negativ_coverage = negativ_hits / all_words_negativ.count.to_f
 
     coverage = {
-      positive: lexicon_coverage_positive,
-      negative: lexicon_coverage_negative
+      positive: {
+        coverage: positiv_coverage,
+        hits: positiv_hits,
+        word_total: all_words_positiv.count
+      },
+      negative: {
+        coverage: negativ_coverage,
+        hits: negativ_hits,
+        word_total: all_words_negativ.count
+      }
     }
     coverage
   end
 end
 
 puts "Lexicon coverage:"
-puts "positive Nokia reviews: #{ "%.2f" % (Coverage.lexicon_coverage[:positive] * 100) }%"
-puts "negative Nokia reviews: #{ "%.2f" % (Coverage.lexicon_coverage[:negative] * 100) }%"
+
+puts "positive Nokia reviews: #{ "%.2f" % (Coverage.stats[:positive][:coverage] * 100) }% \
+(#{ Coverage.stats[:positive][:hits] }/#{ Coverage.stats[:positive][:word_total]})"
+
+puts "negative Nokia reviews: #{ "%.2f" % (Coverage.stats[:negative][:coverage] * 100) }% \
+(#{ Coverage.stats[:negative][:hits] }/#{ Coverage.stats[:negative][:word_total]})"
