a7ad70aa4dcc6a34aed3ea91ddbb9925070b1758
Greg Muszynski <g.muszynski.12@abdn.ac.uk>
2015-11-10 22:31:05 +0000

    Added command line option -e to stop exection on incorrect result

--- nokia.rb
+++ nokia.rb
@@ -1,10 +1,12 @@
 require_relative "computational_sentiment_classifier"
 
+FAIL_ON_ERROR = ARGV.any? { |arg| arg =~ /\-e/ }
+
 nokia_positive = File.open("nokia-pos.txt").read.lines
 nokia_negative = File.open("nokia-neg.txt").read.lines
 
-results_positive = []
-results_negative = []
+results_positive = { "+" => 0, "-" => 0, "~" => 0, "_" => 0 }
+results_negative = { "+" => 0, "-" => 0, "~" => 0, "_" => 0 }
 
 classifier = CSC.new
 
@@ -13,34 +15,33 @@ nokia_positive.each do |review|
   classifier.load_stanford_parse_from "buffer.txt"
 
   result = classifier.calculate_polarity
-  results_positive.push result
-  # if result != "+"
-  #   puts "---\nINCORRECT LABEL: classified as #{result}"
-  #   puts review
-  #   puts "---"
-  #   abort
-  # end
+  results_positive[result] += 1
+
+  if FAIL_ON_ERROR && (result != "+")
+    puts "---\nINCORRECT LABEL: classified as '#{result}'"
+    puts review
+    puts "---"
+    abort
+  end
 end
 
-# nokia_negative.each do |review|
-#   buffer_file = File.open("buffer.txt","w") { |file| file.write review }
-#   classifier.load_stanford_parse_from "buffer.txt"
+nokia_negative.each do |review|
+  buffer_file = File.open("buffer.txt","w") { |file| file.write review }
+  classifier.load_stanford_parse_from "buffer.txt"
 
-#   result = classifier.calculate_polarity
-#   results_negative.push result
-#   if result != "-"
-#     puts "\nThis is where it gives a wrong answer"
-#     puts review
-#     abort
-#   end
-# end
+  result = classifier.calculate_polarity
+  results_negative[result] += 1
 
-correct_positive = results_positive.count "+"
-false_negatives = restuls_positive.count "-"
-# correct_negative = results_negative.count "-"
-# false_positives = results_negative.count "+"
+  if FAIL_ON_ERROR && (result != "-")
+    puts "---\nINCORRECT LABEL: classified as '#{result}'"
+    puts review
+    puts "---"
+    abort
+  end
+end
 
-puts correct_positive
-# puts correct_negative
-# puts false_positives
-puts false_negatives
+puts "POSITIVE"
+puts results_positive
+
+puts "NEGATIVE"
+puts results_negative
